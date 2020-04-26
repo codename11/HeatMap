bf64c1851fc2950bc080bdd9c5896cff5b48cb15
Veljko <veljkos82@gmail.com>
2020-04-26 20:23:23 +0200

    It passes all the tests. Still needs ticks on legend.

--- heatmap5.js
+++ heatmap5.js
@@ -18,7 +18,7 @@ $(document).ready( () => {
             let svg = d3.select("#demo")
                 .append("svg")
                 .attr("width", w + m.left*2)
-                .attr("height", h + m.top + m.bottom*3);
+                .attr("height", h + m.top + m.bottom*5);
 
             //Translating axises to accomodate for margin.If we don't, ticks and numbers would look cut of.
             const g = svg.append("g").attr("transform", "translate(" + m.left*2 + ","+m.top*3+")");
@@ -32,21 +32,32 @@ $(document).ready( () => {
             });
 
             let date = dataset.monthlyVariance.map((item, i) => {
-                return new Date(item.year, item.month-1);
+                return new Date(item.year, item.month-1).getFullYear();
             });
             
+            let yearsNoDuplicates = years.filter((item, i) => {
+                return years.indexOf(item) === i;
+            })
+            //console.log(yearsNoDuplicates.length*12);
+
             //Calculating year.
             let minYear = new Date(d3.min(date));//Calculating first year in dataset.
             let maxYear = new Date(d3.max(date));//Calculating last year in dataset.
-            console.log(minYear, maxYear);
 
             // Build X scales and axis:
-            let x = d3.scaleTime()
+            let x = d3.scaleBand()
                 .range([ 0, w ])
-                .domain([minYear,maxYear]);
+                .domain(yearsNoDuplicates);
 
+            /*
+            Same result.
+            console.log(x.domain());
+            console.log(yearsNoDuplicates);
+            */
             //Drawing y axis.
-            const xAxis = d3.axisBottom(x);
+            const xAxis = d3.axisBottom(x).tickValues(x.domain().filter((d,i) => { 
+                return d%10===0;
+            }));
             
             //Appending x axis to chart.
             g.append("g")
@@ -54,8 +65,6 @@ $(document).ready( () => {
                 .attr("class", "tick axis")
                 .attr("id", "x-axis")
                 .call(xAxis);
-
-            //console.log(minMonth, maxMonth);
 
             let monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
             let y = d3.scaleBand()
@@ -69,9 +78,18 @@ $(document).ready( () => {
                 .attr("id", "y-axis")
                 .call(yAxis);
 
-            let rectWidth = w / dataset.monthlyVariance.length;
+            let rectWidth = (w-m.left-m.right) / dataset.monthlyVariance.length;
             let rectHeight = h / 12;
 
+            let temps = dataset.monthlyVariance.map((item, i) => {
+                return item.variance;
+            });
+            let minTemp = d3.min(temps)+baseTemp;
+            let maxTemp = d3.max(temps)+baseTemp;
+            let colors = d3.scaleLinear()
+                .range(["yellow", "red", "purple", "navy"])
+                .domain([minTemp, maxTemp]);
+            
             //Creating tooltip element.
             const tooltip = d3.select('#demo')
                 .append('div')
@@ -84,24 +102,16 @@ $(document).ready( () => {
                 .enter()
                 .append("rect")
                 .attr("x", (d,i) => {
-                    
-                   
-                    return rectWidth*i;
-                    
-                    
+                    return x(date[i]);
                 })
                 .attr("y", (d,i) => { 
-                    console.log(d.month);
                     return  y(monthNames[d.month-1]);
-                    
                 })
                 .attr("width", (d, i) =>{
-                    //return (i*rectWidth);
-                    //return x.bandwidth();
-                    return 5;
+                    
+                    return rectWidth*12;
                 })
                 .attr("height", (d, i) =>{
-                    //return y.bandwidth();
                     return rectHeight;
                 })
                 .attr("data-year", (d,i) => {
@@ -116,18 +126,79 @@ $(document).ready( () => {
                     return baseTemp+d.variance;
                 })
                 .attr("class", "cell")
+                .style('fill', (d, i) => {
+                    return colors(d.variance+baseTemp);
+                })
                 .on('mouseover', (d, i) => {
                     tooltip.style('opacity', 1);
                     tooltip.html("<div style='margin-bottom: 5px;'>Year: "+d.year+" Month: "+d.month+" variance: "+d.variance+"</div>");
+                    tooltip.attr('data-year', d.year);
+                    tooltip.attr("left", "50");
+                    tooltip.attr("top", "10");
                 })
                 .on('mouseout', (d) => {
                     tooltip.style('opacity', 0);
                 });
 
+                //Draw the Heat Label:
+                svg.append("text")
+                    .attr("id", "title")
+                    .attr("class", "headline")
+                    .attr("x", w / 2)
+                    .attr("y", m.top)
+                    .attr("font-family", "sans-serif")
+                    .attr("fill", "green")
+                    .attr("text-anchor", "middle")
+                    .text("Monthly Global Land-Surface Temperature"); 
+
+                //Draw the Heat Sub-Label:
+                svg.append("text")
+                    .attr("id", "description")
+                    .attr("class", "headline1")
+                    .attr("x", w / 2)
+                    .attr("y", m.top*2)
+                    .attr("font-family", "sans-serif")
+                    .attr("fill", "green")
+                    .attr("text-anchor", "middle")
+                    .text(minYear.getFullYear()+" - "+maxYear.getFullYear()+": base temperature "+baseTemp+"°C");
+                    
+                //Creating legend element.
+                const legend = svg.append('g')
+                    .attr('id', 'legend')
+                    .attr("transform", "translate("+m.left*2+", 0)");
+
+                legend.selectAll("rect")
+                    .data(colors.range())
+                    .enter()
+                    .append("rect")
+                    .attr("x", (d, i) => {
+                        return ((m.left+2)*i);
+                    })
+                    .attr("y", h+m.top*4)
+                    .attr("width", m.left)
+                    .attr("height", m.left)
+                    .attr("fill", (d, i) => {
+                        return d;
+                    });
+                
+                console.log(colors.range());
+
         },
         error: (xhr, ajaxOptions, thrownError) => {
 
             console.log(xhr, ajaxOptions, thrownError);
+            
+        }
+        
+    });
+
+    
+    document.addEventListener("mouseover", (e) => {
+
+        if(e.target.className.baseVal === "cell"){
+            
+            document.getElementById("tooltip").style.left = e.clientX - 120;
+            document.getElementById("tooltip").style.top = e.clientY - 80;
             
         }
         
